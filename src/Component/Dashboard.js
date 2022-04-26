b5b1c0d428f656053ae4dbe98409682533151bf9
Vachay <nguyentai02042001@gmail.com>
2022-04-26 23:32:38 +0700

    first commit

---
+++ src/Component/Dashboard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+export default function Dashboard() {
+    return (
+        <div className='container dashboard'>
+            <div className='header-main'>
+                <a href='#' className='logo float-left'> <img src={require("./asset/images/logo.jpg") alt='logo'></img></a>
+
+                <div className='float-right menu'>
+                    <ul style={{ display: 'none' }}>
+                        <li className='nav-link'><Link to='/home'>Home</Link></li>
+                        <li className='nav-link'><Link to='/new-articles'><span><i className="fas fa-edit"></i></span>New articles</Link></li>
+                        <li className='nav-link'><Link to='/setting'><span><i className="fas fa-cog"></i></span>Setting</Link></li>
+
+                    </ul >
+                    <ul style={{ display: 'inherit' }}>
+                        <li className='nav-link'><Link to='/home'>Home</Link></li>
+                        <li className='nav-link'><Link to='/new-articles'><span><i className="fas fa-edit"></i></span>New articles</Link></li>
+                        <li className='nav-link'><Link to='/setting'><span><i className="fas fa-cog"></i></span>Setting</Link></li>
+                        <li className='nav-link'><Link to='/setting'><span><i className="fas fa-user"></i></span>User</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
